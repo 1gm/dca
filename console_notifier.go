5208796d8e919232677120d75e1a42c38a0c7964
George Mokbel <georgemokbel@gmail.com>
2025-03-16 11:45:54 -0400

    Add initial console notifier implementation

---
+++ console_notifier.go
@@ -0,0 +1,37 @@
+package dca
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+)
+
+// ConsoleNotifier is a stub implementation of a notifier used for local development.
+type ConsoleNotifier struct {
+	Destination io.Writer
+}
+
+// NewConsoleWriter creates a ConsoleNotifier configured to write output to stdout.
+func NewConsoleWriter() *ConsoleNotifier {
+	return &ConsoleNotifier{
+		Destination: os.Stdout,
+	}
+}
+
+func (n *ConsoleNotifier) Notify(_ context.Context, response ExecuteOrderResponse) (err error) {
+	defer AddErr(&err, "ConsoleNotifier.Notify")
+	msg := fmt.Sprintf("bought %.8f bitcoin for %.2f (paid %.2f fee)", response.VolumePurchased, response.Cost, response.Fee)
+	if _, err = fmt.Fprintf(n.Destination, msg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (n *ConsoleNotifier) NotifyFailure(_ context.Context, f error) (err error) {
+	defer AddErr(&err, "ConsoleNotifier.NotifyFailure")
+	if _, err = fmt.Fprintf(n.Destination, fmt.Sprintf("failed to buy bitcoin: %v", f)); err != nil {
+		return err
+	}
+	return nil
+}
